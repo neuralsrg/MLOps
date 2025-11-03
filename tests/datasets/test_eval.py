d2aac9403a059471070a079a7c76808ad7de7c17
Sergei Makeev <jsetup@hotmail.com>
2025-11-03 23:16:09 +0300

    Add dataset tests

---
+++ tests/datasets/test_eval.py
@@ -0,0 +1,45 @@
+import pytest
+
+import torch
+
+from logq.utils.utils import load_config
+from logq.utils.dataset import get_val_dataloader, get_test_dataloader, get_num_items, MAX_RATED_ITEMS
+
+
+@pytest.fixture(params=[{'cfg_path': 'logq/configs/ml1m_sasrec.py', 'get_ds': get_val_dataloader},
+                        {'cfg_path': 'logq/configs/ml1m_other.py', 'get_ds': get_val_dataloader},
+                        {'cfg_path': 'logq/configs/ml1m_sasrec.py', 'get_ds': get_test_dataloader},
+                        {'cfg_path': 'logq/configs/ml1m_other.py', 'get_ds': get_test_dataloader}])
+def eval_dataloader(request):
+    params = request.param
+    config = load_config(params['cfg_path'])
+    eval_dataloader = get_val_dataloader(
+        dataset_name=config.dataset_name,
+        batch_size=config.eval_batch_size,
+        max_length=config.sequence_length
+    )
+    return eval_dataloader, config
+
+def test_eval_dataloader(eval_dataloader):
+    eval_dataloader, config = eval_dataloader
+    batch = next(iter(eval_dataloader))
+
+    assert len(batch) == 3                                  # [input, rated, output]
+
+    assert batch[0].shape == (config.eval_batch_size, config.sequence_length)
+    assert batch[1].shape == (config.eval_batch_size, MAX_RATED_ITEMS)
+    assert batch[2].shape == (config.eval_batch_size, )
+
+    num_items = get_num_items(config.dataset_name)
+
+    for i in range(config.eval_batch_size):
+        inputs = set(batch[0][i].tolist())
+        if num_items + 1 in inputs:
+            inputs.remove(num_items + 1)
+        rated = set(batch[1][i].tolist())
+        assert inputs.issubset(rated)
+
+    for i in range(len(batch)):
+        assert batch[i].dtype == torch.int64
+        assert torch.min(batch[i]).item() >= 1              # item_ids are in [1, num_items]
+        assert torch.max(batch[0]).item() <= num_items + 1  # num_items + 1 is PAD
---
+++ tests/datasets/test_train.py
@@ -0,0 +1,39 @@
+import pytest
+
+import torch
+
+from logq.utils.utils import load_config
+from logq.utils.dataset import get_train_dataloader, get_num_items
+
+
+@pytest.fixture(params=[{'cfg_path': 'logq/configs/ml1m_sasrec.py'},
+                        {'cfg_path': 'logq/configs/ml1m_other.py'}])
+def train_dataloader(request):
+    params = request.param
+    config = load_config(params['cfg_path'])
+    train_dataloader = get_train_dataloader(
+        dataset_name=config.dataset_name,
+        batch_size=config.train_batch_size,
+        max_length=config.sequence_length,
+        train_neg_per_positive=config.negs_per_pos
+    )
+    return train_dataloader, config
+
+def test_train_dataloader(train_dataloader):
+    train_dataloader, config = train_dataloader
+    batch = next(iter(train_dataloader))
+
+    assert len(batch) == 2
+
+    assert batch[0].shape == (config.train_batch_size, config.sequence_length + 1)
+    assert batch[1].shape == (config.train_batch_size, config.sequence_length + 1, config.negs_per_pos)
+
+    assert batch[0].dtype == torch.int64
+    assert batch[1].dtype == torch.int64
+
+    assert torch.min(batch[0]).item() >= 1              # item_ids are in [1, num_items]
+    assert torch.min(batch[1]).item() >= 1              # item_ids are in [1, num_items]
+
+    num_items = get_num_items(config.dataset_name)
+    assert torch.max(batch[0]).item() <= num_items + 1  # num_items + 1 is PAD
+    assert torch.max(batch[1]).item() <= num_items + 1  # num_items + 1 is PAD
